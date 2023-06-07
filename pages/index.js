2339f45199478b83bf3c70f9163ff203b2dbc97a
Phước Nguyễn Viết <nguyenvietphuoc24.12@gmail.com>
2023-06-07 08:58:00 +0700

    Upload some files includes of components/bio.js and pages/index.js and edited sources package.json

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import NextLink from 'next/link'
+import {
+  Link, 
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import { 'ChevronRightIcon, EmailIcon' } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+
